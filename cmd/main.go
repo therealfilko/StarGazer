8d82a5bf431935eb634ef4c874da40184bff4694
TheRealFilko <fn.filipnikolic@gmail.com>
2024-03-28 22:52:26 +0100

    Added go-github

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"os"
+
+	"github.com/google/go-github/v60/github"
+)
+
 func main()  {
+    startDate := os.Args[1]
+    endDate := os.Args[2]
+
+    search, err := github.SearchService{}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/therealfilko/StarGazer
 
 go 1.21.1
+
+require github.com/google/go-github/v60 v60.0.0 // indirect
