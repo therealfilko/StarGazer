90249bd85fa19946c30a04f0a7b999429245d467
TheRealFilko <fn.filipnikolic@gmail.com>
2024-03-28 22:58:14 +0100

    Work with TOKEN

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/google/go-github/v60/github"
 )
 
+// 1. ich muss env richtig einbinden also mein Token
+// 2. danach authentifizieren
+// 3. Struct definieren
+// 4. Suche definieren
+// 5. testen
 func main()  {
     startDate := os.Args[1]
     endDate := os.Args[2]
+    token := os.Getenv("AUTH_TOKEN")
 
-    search, err := github.SearchService{}
+    fmt.Println(startDate)
+    fmt.Println(endDate)
+    fmt.Println(token)
+
 }
