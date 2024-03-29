1b6a11c4f4663d02ea0ef32dc9e6d8ada72b1263
TheRealFilko <fn.filipnikolic@gmail.com>
2024-03-29 14:36:26 +0100

    Refactor some code

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/google/go-github/v60/github"
+	"github.com/joho/godotenv"
 )
 
-// 1. ich muss env richtig einbinden also mein Token
-// 2. danach authentifizieren
-// 3. Struct definieren
-// 4. Suche definieren
-// 5. testen
+// 1. ich muss env richtig einbinden also mein Token | DONE
+// 2. danach authentifizieren | DONE
+// 3. Suche definieren
+// 4. testen
+
 func main()  {
+    token := getToken()
+    client := github.NewClient(nil).WithAuthToken(token)
+    testAuthentication(client)
+
     startDate := os.Args[1]
     endDate := os.Args[2]
-    token := os.Getenv("AUTH_TOKEN")
 
-    fmt.Println(startDate)
-    fmt.Println(endDate)
-    fmt.Println(token)
+    fmt.Printf("Anfangsdatum: %s\n", startDate)
+    fmt.Printf("Enddatum: %s\n", endDate)
+}
 
+func getToken() string  {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    return os.Getenv("AUTH_TOKEN")
+}
+
+func testAuthentication(client *github.Client) {
+    user, _, err := client.Users.Get(context.Background(), "")
+    if err != nil {
+        log.Fatalf("Authentifizierungsfehler: %v", err)
+    } else {
+        fmt.Printf("Erfolgreich authentifiziert als Benutzer: %s\n", *user.Login)
+    }
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/therealfilko/StarGazer
 
 go 1.21.1
 
-require github.com/google/go-github/v60 v60.0.0 // indirect
+require (
+	github.com/google/go-github/v60 v60.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
