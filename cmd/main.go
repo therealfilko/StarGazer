62be0a37f64b58573fe2a32b3770e2d4c124d388
TheRealFilko <fn.filipnikolic@gmail.com>
2024-03-29 19:00:22 +0100

    Added the search function

--- cmd/main.go
+++ cmd/main.go
@@ -18,21 +18,39 @@ import (
 func main()  {
     token := getToken()
     client := github.NewClient(nil).WithAuthToken(token)
-    testAuthentication(client)
 
     startDate := os.Args[1]
     endDate := os.Args[2]
 
     fmt.Printf("Anfangsdatum: %s\n", startDate)
     fmt.Printf("Enddatum: %s\n", endDate)
+
+    // Funktion anpassen bis es geht
+    searchRepositories(client)
 }
 
-func getToken() string  {
+func getToken() string  { 
     err := godotenv.Load()
     if err != nil {
         log.Fatal("Error loading .env file")
     }
     return os.Getenv("AUTH_TOKEN")
+}
+
+func searchRepositories(client *github.Client) {
+    ctx := context.Background()
+    query := "go-github"
+
+    opts := &github.SearchOptions{Sort: "stars", Order: "desc"}
+    result, _, err := client.Search.Repositories(ctx, query, opts) // 2 parameter ist query, hiermit kann ich halt die suche starten
+    if err != nil {
+        log.Fatal("Couldn't find it")
+    }
+
+    fmt.Printf("Gefunden Repos: %d\n", *result.Total)
+    for _, repo := range result.Repositories {
+        fmt.Printf("Name: %s, Sterne: %d, URL: %s\n", *repo.Name, *repo.StargazersCount, *repo.HTMLURL)
+    }
 }
 
 func testAuthentication(client *github.Client) {
